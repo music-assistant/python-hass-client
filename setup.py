ce677b0c8176d984cba6a79c9c092c5deca85982
Marcel van der Veldt <m.vanderveldt@outlook.com>
2020-12-30 01:11:43 +0100

    fix for windows installs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from setuptools import find_packages, setup
 
-VERSION = "0.0.7"
+VERSION = "0.0.8"
 
 PROJECT_DIR = Path(__file__).parent.resolve()
 README_FILE = PROJECT_DIR / "README.md"
